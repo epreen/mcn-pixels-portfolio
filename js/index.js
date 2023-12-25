bcd0499ddaaef1144197094a25e0846ea478c9bd
Epreen Njolomole <125865502+epreen@users.noreply.github.com>
2023-12-25 13:28:32 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
+// Event listener for changing the navigation bar style on scroll
 window.addEventListener
 (
     'scroll', () =>
     {
-        document.querySelector('nav').classList.toggle('scrolled',
-        window.scrollY > 0)
+        document.querySelector('nav').classList.toggle
+        (
+            'scrolled', window.scrollY > 0    
+        );
     }
-)
+);
 
+// Event listener for setting the theme on page load
 window.addEventListener
 (
     'load', () =>
     {
         document.body.className = window.localStorage.getItem('theme');
     }
-)
+);
 
-var swiper = new Swiper(".mySwiper",
+// Swiper initialization
+var swiper = new Swiper
+(
+    ".mySwiper",
     {
         slidesPerView: 1,
         spaceBetween: 20,
@@ -38,50 +45,124 @@ var swiper = new Swiper(".mySwiper",
             }
         }
     }
-)
+);
 
-var root = document.querySelector(':root');
+// DOM elements
+var root = document.documentElement;
 
 const theme_button = document.querySelector('.logo');
 const theme_model = document.querySelector('.theme_panel');
+const color_settings = document.querySelectorAll('.color_options span');
+const light_mode = document.querySelector('.background-1');
+const dark_mode = document.querySelector('.background-2');
+const icon = document.querySelector('#favicon');
 
-const color_settings = document.querySelectorAll('.color_options span')
-
-const light_mode = document.querySelector('.background-1')
-const dark_mode = document.querySelector('.background-2')
-
-const red_theme = document.querySelector('.color-2')
-
-const ViewThemeModel = () => {
-        theme_model.style.display = 'grid';
-}
-
-const HideThemeModel = (e) => {
-    if(e.target.classList.contains('theme_panel')){
-        theme_model.style.display = 'none';
+// Function to change favicon
+const changeFavicon = (PrimaryHue) =>
+{
+    if (PrimaryHue === 210)
+    {
+        icon.href = 'mcn-icon-blue.svg';
     }
-}
+    
+    else if (PrimaryHue === 710)
+    {
+        icon.href = 'mcn-icon-red.svg';
+    }
+    
+    else if (PrimaryHue === 110)
+    {
+        icon.href = 'mcn-icon-green.svg';
+    }
+    
+    else if (PrimaryHue === 1110)
+    {
+        icon.href = 'mcn-icon-orange.svg';
+    }
+    
+    else if (PrimaryHue === 1710)
+    {
+        icon.href = 'mcn-icon-magenta.svg';
+    }
+    
+    else if (PrimaryHue === 2210)
+    {
+        icon.href = 'mcn-icon-yellow.svg';
+    }
+};
+
+// Event listener for color options
+color_settings.forEach
+(
+    color =>
+    {
+        color.addEventListener
+        (
+            'click', () =>
+            {
+                if (color.classList.contains('color-1'))
+                {
+                    PrimaryHue = 210;
+                }
+                
+                else if (color.classList.contains('color-2'))
+                {
+                    PrimaryHue = 710;
+                }
+                
+                else if (color.classList.contains('color-3'))
+                {
+                    PrimaryHue = 110;
+                }
+                
+                else if (color.classList.contains('color-4'))
+                {
+                    PrimaryHue = 1110;
+                }
+                
+                else if (color.classList.contains('color-5'))
+                {
+                    PrimaryHue = 1710;
+                }
+                
+                else if (color.classList.contains('color-6'))
+                {
+                    PrimaryHue = 2210;
+                }
+
+                changeFavicon(PrimaryHue);
+
+                root.style.setProperty('--primary-color-hue', PrimaryHue);
+            }
+        );
+    }
+);
+
+theme_button.addEventListener
+(
+    'click', () =>
+    {
+        theme_model.style.display = 'grid';
+    }
+);
+
+theme_model.addEventListener
+(
+    'click', (e) =>
+    {
+        if (e.target.classList.contains('theme_panel'))
+        {
+            theme_model.style.display = 'none';
+        }
+    }
+);
 
 dark_mode.addEventListener
 (
     'click', () =>
     {
-        let bodyclass = document.body.className;
-
-        if(!bodyclass)
-        {
-            bodyclass = 'dark';
-            document.body.className = bodyclass;
-
-            window.localStorage.setItem('theme', bodyclass);
-        }
-        else
-        {
-            bodyclass = "dark";
-            document.body.className = bodyclass;
-
-            window.localStorage.setItem('theme', bodyclass);
-        }
+        document.body.className = 'dark';
+        window.localStorage.setItem('theme', 'dark');
     }
 );
 
@@ -89,45 +170,7 @@ light_mode.addEventListener
 (
     'click', () =>
     {
-        let bodyclass = document.body.className;
-
-        if(!bodyclass)
-        {
-            bodyclass = 'light';
-            document.body.className = bodyclass;
-
-            window.localStorage.setItem('theme', bodyclass);
-        }
-        else
-        {
-            bodyclass = "light";
-            document.body.className = bodyclass;
-
-            window.localStorage.setItem('theme', bodyclass);
-        }
+        document.body.className = 'light';
+        window.localStorage.setItem('theme', 'light');
     }
 );
-
-color_settings.forEach(color => {
-    color.addEventListener('click', () => {
-        let PrimaryHue
-        if(color.classList.contains('color-1')){
-            PrimaryHue = 210;
-        } else if(color.classList.contains('color-2')){
-            PrimaryHue = 710;
-        } else if(color.classList.contains('color-3')){
-            PrimaryHue = 110;
-        } else if(color.classList.contains('color-4')){
-            PrimaryHue = 1110;
-        } else if(color.classList.contains('color-5')){
-            PrimaryHue = 1710;
-        } else if(color.classList.contains('color-6')){
-            PrimaryHue = 2210;
-        }
-
-        root.style.setProperty('--primary-color-hue', PrimaryHue);
-    })
-})
-
-theme_button.addEventListener('click', ViewThemeModel);
-theme_model.addEventListener('click', HideThemeModel);
